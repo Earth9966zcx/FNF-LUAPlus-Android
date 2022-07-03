8fa082b5a4b2f937fcb7069368e64f27e643ffb6
Pambi69420 <chachnan789@gmail.com>
2022-07-03 11:56:41 +0700

    Added Main menu sides option

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -118,7 +118,16 @@ class MainMenuState extends MusicBeatState
 			menuItem.animation.addByPrefix('selected', optionShit[i] + " white", 24);
 			menuItem.animation.play('idle');
 			menuItem.ID = i;
-			menuItem.screenCenter(X);
+			//funny options
+			if(ClientPrefs.iconsSide == 'Middle') {
+			  menuItem.screenCenter(X);
+			}
+			if(ClientPrefs.iconsSide == 'Left') {
+			  menuItem.x += 100;
+			}
+			if(ClientPrefs.iconsSide == 'Right') {
+			  menuItem.x += 650;
+			}
 			menuItems.add(menuItem);
 			var scr:Float = (optionShit.length - 4) * 0.135;
 			if(optionShit.length < 6) scr = 0;
@@ -273,7 +282,9 @@ class MainMenuState extends MusicBeatState
 
 		menuItems.forEach(function(spr:FlxSprite)
 		{
-			spr.screenCenter(X);
+		  if(ClientPrefs.iconsSide == 'Middle') {
+		    spr.screenCenter(X);
+		  }
 		});
 	}
 
